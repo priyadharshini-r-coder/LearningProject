65a6901ee84ea168e2123304d2cfe4c405580762
ManoLakshathaSrinivasaMoorthi <mano.s@indiumsoft.com>
2021-12-28 18:42:17 +0530

    Google Map

--- app/src/main/java/com/example/learningproject/MapsActivity.java
+++ app/src/main/java/com/example/learningproject/MapsActivity.java
@@ -1,100 +1,294 @@
 package com.example.learningproject;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.databinding.DataBindingUtil;
-
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
-import android.text.Html;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.ActionMode;
+import androidx.appcompat.widget.AppCompatEditText;
+import androidx.core.app.ActivityCompat;
+import androidx.databinding.DataBindingUtil;
 
 import com.example.learningproject.databinding.ActivityMapsBinding;
+import com.example.learningproject.services.Utils;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 
-public class MapsActivity extends AppCompatActivity {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
+    /* //Initialise variables
+     Button btnLocation;
+     public ActivityMapsBinding binding;
+     TextView textView1, textView2, textView3, textView4, textView5;
+     FusedLocationProviderClient fusedLocationProviderClient;
 
-    //Initialise variables
-   // Button btnLocation;
-    public ActivityMapsBinding binding;
-   // TextView textView1, textView2, textView3, textView4, textView5;
+     @Override
+     protected void onCreate(Bundle savedInstanceState) {
+         super.onCreate(savedInstanceState);
+         binding = DataBindingUtil.setContentView(this, R.layout.activity_maps);
+
+         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+         binding.btLocation.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 //check permission
+                 if (ActivityCompat.checkSelfPermission(MapsActivity.this,
+                         Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                     //when permission granted
+                     getLocation();
+
+                 } else {
+                     ActivityCompat.requestPermissions(MapsActivity.this, new String[]
+                             {Manifest.permission.ACCESS_FINE_LOCATION}, 44);
+                 }
+             }
+         });
+     }
+
+     private void getLocation() {
+         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+             // TODO: Consider calling
+             //    ActivityCompat#requestPermissions
+             // here to request the missing permissions, and then overriding
+             //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+             //                                          int[] grantResults)
+             // to handle the case where the user grants the permission. See the documentation
+             // for ActivityCompat#requestPermissions for more details.
+             return;
+         }
+         fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+             @Override
+             public void onComplete(@NonNull Task<Location> task) {
+                 Location location = task.getResult();
+                 if (location != null) {
+                     Geocoder geocoder = new Geocoder(MapsActivity.this, Locale.getDefault());
+                     //Initialise address List
+                     try {
+                         List<Address> addressList = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+                         //setLatitude
+                         binding.text.setText(Html.fromHtml("<font color='#6200EE'><b>Latitude:</b><br></font>" + addressList.get(0).getLatitude()));
+                         //setLongitude
+                         binding.text1.setText(Html.fromHtml("<font color='#6200EE'><b>Longitude:</b><br></font>" + addressList.get(0).getLongitude()));
+
+                         //setCountryName
+                         binding.textView2.setText(Html.fromHtml("<font color='#6200EE'><b>Latitude:</b><br></font>" + addressList.get(0).getCountryCode()));
+
+                         //setlocality
+                         binding.textView3.setText(Html.fromHtml("<font color='#6200EE'><b>Locality:</b><br></font>" + addressList.get(0).getLocality()));
+
+                         //address
+                         binding.textView4.setText(Html.fromHtml("<font color='#6200EE'><b>Address:</b><br></font>" + addressList.get(0).getAddressLine(0)));
+                     }
+                     //set latitude on Textview
+
+                     catch (IOException e) {
+                         e.printStackTrace();
+                     }
+                 }
+
+             }
+         });
+     }
+
+
+ }*/
+
+    private static final String TAG = "MapsActivity";
+    Location currentLocation;
     FusedLocationProviderClient fusedLocationProviderClient;
+    private ActivityMapsBinding binding;
+
+    private static final int REQUEST_CODE = 101;
+    private BottomSheetBehavior bottomSheetBehavior;
+    private GoogleMap mMap;
+    TextView locationLabel;
+    AppCompatEditText locationSearch;
+
+    @Override
+    public void onSupportActionModeStarted(@NonNull ActionMode mode) {
+        super.onSupportActionModeStarted(mode);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_maps);
+      binding= DataBindingUtil. setContentView(this,R.layout.activity_maps);
+
+        initComponent();
+
+        Utils.setSystemBarColor(this, android.R.color.white);
+        Utils.setSystemBarLight(this);
 
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-        binding.btLocation.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //check permission
-                if (ActivityCompat.checkSelfPermission(MapsActivity.this,
-                        Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                    //when permission granted
-                    getLocation();
+        fetchLocation();
 
-                } else {
-                    ActivityCompat.requestPermissions(MapsActivity.this, new String[]
-                            {Manifest.permission.ACCESS_FINE_LOCATION}, 44);
-                }
-            }
-        });
+        locationLabel = findViewById(R.id.loc_txt);
     }
 
-    private void getLocation() {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-
+    private void fetchLocation() {
+        if (ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
             return;
         }
-        fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
-            @Override
-            public void onComplete(@NonNull Task<Location> task) {
-                Location location = task.getResult();
-                if (location != null) {
-                    Geocoder geocoder = new Geocoder(MapsActivity.this, Locale.getDefault());
-                    //Initialise address List
-                    try {
-                        List<Address> addressList = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
-                        //setLatitude
-                        binding.text.setText(Html.fromHtml("<font color='#6200EE'><b>Latitude:</b><br></font>" + addressList.get(0).getLatitude()));
-                        //setLongitude
-                        binding.text1.setText(Html.fromHtml("<font color='#6200EE'><b>Longitude:</b><br></font>" + addressList.get(0).getLongitude()));
-
-                        //setCountryName
-                        binding.textView2.setText(Html.fromHtml("<font color='#6200EE'><b>Latitude:</b><br></font>" + addressList.get(0).getCountryCode()));
-
-                        //setlocality
-                        binding.textView3.setText(Html.fromHtml("<font color='#6200EE'><b>Locality:</b><br></font>" + addressList.get(0).getLocality()));
-
-                        //address
-                        binding.textView4.setText(Html.fromHtml("<font color='#6200EE'><b>Address:</b><br></font>" + addressList.get(0).getAddressLine(0)));
-                    }
-                    //set latitude on Textview
-
-                    catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-
+        Task<Location> task = fusedLocationProviderClient.getLastLocation();
+        task.addOnSuccessListener(location -> {
+            if (location != null) {
+                currentLocation = location;
+               getAddressFromLocation(currentLocation, getApplicationContext(), new GeoCoderHandler());
+                SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+                assert supportMapFragment != null;
+                supportMapFragment.getMapAsync(MapsActivity.this);
             }
         });
     }
 
+
+   public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        LatLng latLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+        MarkerOptions markerOptions = new MarkerOptions();
+       markerOptions.position(latLng);
+        markerOptions.title("Current Position");
+        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW));
+        googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 17.0f));
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        googleMap.setPadding(500, 1100, 0, 0);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        googleMap.setMyLocationEnabled(true);
+        googleMap.getUiSettings().setZoomControlsEnabled(false);
+        googleMap.getUiSettings().setMyLocationButtonEnabled(true);
+        googleMap.addMarker(markerOptions);
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                fetchLocation();
+            }
+        }
+    }
+
+
+    public static void getAddressFromLocation(final Location location, final Context context, final Handler handler) {
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                Geocoder geocoder = new Geocoder(context, Locale.getDefault());
+                String result = null;
+                try {
+                    List<Address> list = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+                    if (list != null && list.size() > 0) {
+                        Address address = list.get(0);
+                        // sending back first address line and locality
+                        result = address.getAddressLine(0) + ", " + address.getLocality() + ", " + address.getCountryName();
+                    }
+                } catch (IOException e) {
+                    Log.e(TAG, "Impossible to connect to Geocoder", e);
+                } finally {
+                    Message msg = Message.obtain();
+                    msg.setTarget(handler);
+                    if (result != null) {
+                        msg.what = 1;
+                        Bundle bundle = new Bundle();
+                        bundle.putString("address", result);
+                        msg.setData(bundle);
+                    } else
+                        msg.what = 0;
+                    msg.sendToTarget();
+                }
+            }
+        };
+        thread.start();
+    }
+    private class GeoCoderHandler extends Handler {
+        @Override
+        public void handleMessage(Message message) {
+            String result;
+            if (message.what == 1) {
+                Bundle bundle = message.getData();
+                result = bundle.getString("address");
+            } else {
+                result = null;
+            }
+            locationLabel.setText(result);
+            //Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void searchLocation(View view) {
+        locationSearch = findViewById(R.id.et_search);
+        String location = locationSearch.getText().toString();
+        List<Address> addressList = null;
+
+        if (location != null || !location.equals("")) {
+            Geocoder geocoder = new Geocoder(this);
+            try {
+                addressList = geocoder.getFromLocationName(location, 1);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            assert addressList != null;
+            Address address = addressList.get(0);
+            LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+            mMap.addMarker(new MarkerOptions().position(latLng).title(location));
+            mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+            Toast.makeText(getApplicationContext(),address.getLatitude()+" "+address.getLongitude(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void initComponent() {
+        // get the bottom sheet view
+        LinearLayout llBottomSheet = findViewById(R.id.bottom_sheet);
+
+        // init the bottom sheet behavior
+        bottomSheetBehavior = BottomSheetBehavior.from(llBottomSheet);
+
+        // change the state of the bottom sheet
+        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+
+        // set callback for changes
+        bottomSheetBehavior.addBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+
+            }
+        });
+    }
 
 }
