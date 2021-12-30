36947dc55497a2da02a545149a769435e800cb4b
Priyadharshini-r-coder <priyadharshini.c@indiumsoft.com>
2021-12-30 18:10:31 +0530

    Current code

---
+++ app/src/main/java/com/example/learningproject/activities/FindDriver.java
@@ -0,0 +1,17 @@
+package com.example.learningproject.activities;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.learningproject.R;
+
+public class FindDriver extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_find_driver );
+    }
+}
