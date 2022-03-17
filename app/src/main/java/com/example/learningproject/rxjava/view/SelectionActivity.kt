6a1cbdc7803534e2719ab23460b1f14ddf43d15a
Priyadharshini-r-coder <priyadharshini.c@indiumsoft.com>
2022-03-17 19:48:27 +0530

    Rxjava map,zip operators

--- app/src/main/java/com/example/learningproject/rxjava/view/SelectionActivity.kt
+++ app/src/main/java/com/example/learningproject/rxjava/view/SelectionActivity.kt
@@ -3,23 +3,28 @@ package com.example.learningproject.rxjava.view
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.learningproject.R
-import com.example.learningproject.databinding.ActivityOperatorsBinding
 import com.example.learningproject.databinding.ActivitySelectionBinding
 import com.example.learningproject.rxjava.search.SearchActivity
 
 class SelectionActivity : AppCompatActivity() {
+
+
     private lateinit var binding: ActivitySelectionBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-      binding= ActivitySelectionBinding.inflate(layoutInflater)
-        val view=binding.root
+        binding = ActivitySelectionBinding.inflate(layoutInflater)
+        val view = binding.root
         setContentView(view)
         binding.operators.setOnClickListener {
-            startActivity(Intent(this,OperatorsActivity::class.java))
+            startActivity(Intent(this@SelectionActivity, OperatorsActivity::class.java))
         }
         binding.search.setOnClickListener {
-            startActivity(Intent(this,SearchActivity::class.java))
+            startActivity(Intent(this@SelectionActivity, SearchActivity::class.java))
         }
     }
 }
+
+
+
